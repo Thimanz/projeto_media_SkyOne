327a276d232d78790d884e79a72db2b783f5bf23
Thiago Manzi <thiago_manzi@hotmail.com>
2023-07-14 20:15:53 -0300

    teste01

--- media_notas/Program.cs
+++ media_notas/Program.cs
@@ -2,4 +2,4 @@
 double num1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Digite a nota do segundo semestre");
 double num2 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine((num1 + num2) / 2);
+Console.WriteLine("Sua média é " + (num1 + num2) / 2);
