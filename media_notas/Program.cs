e8096e8d6299beff92838baaa71e3afc5a4f8c3f
Thiago Manzi <thiago_manzi@hotmail.com>
2023-07-14 20:10:11 -0300

    Adicionar arquivos de projeto.

---
+++ media_notas/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Digite a nota do primeiro semestre");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite a nota do segundo semestre");
+double num2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine((num1 + num2) / 2);
