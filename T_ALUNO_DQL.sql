c04a4d529fff41acf9290df8407dea9f89eacbee
Thiago Manzi <thiago_manzi@hotmail.com>
2023-07-16 19:32:27 -0300

    teste08: select de alunos com seus status sem usar a coluna 'status' da tabela

---
+++ T_ALUNO_DQL.sql
@@ -0,0 +1,9 @@
+use alunosdb;
+
+-- sem usar o dado de status calculando-o dentro do select
+SELECT nome as "Nome do aluno", 
+	   nota1 as "Nota do Primeiro Semestre", 
+       nota2 as "Nota do Segundo Semestre", 
+       media as "Média Anual", ano as "Ano", 
+       CASE WHEN media > 6.5 THEN "Aprovado" ELSE "Reprovado" END AS "Status"
+from t_aluno
